4597d9a0d10bcacf88f6c09d8bf2e6fc92974698
Angtty <angkitty0.0@gmail.com>
2026-09-04 20:40:24 -0500

    Paso 2

--- SkillMatchEjercicio/src/Main.java
+++ SkillMatchEjercicio/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
 
         Vehiculos[] vehiculos = {
                 new Carro(),
@@ -12,5 +13,42 @@ public class Main {
             vehiculo1.detener();
             vehiculo1.combustible();
         }
+=======
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== SELECCIONE UNA OPCION ===");
+        System.out.println("1. Empresa");
+        System.out.println("2. Candidato");
+        int opcion = sc.nextInt();
+        sc.nextLine();
+
+        Usuario usuario = null;
+
+        if (opcion == 1) {
+            Empresa empresa = new Empresa();
+            System.out.println("Ingrese el nombre de la empresa:");
+            empresa.setNombre(sc.nextLine());
+            System.out.println("Ingrese el NIT de la empresa:");
+            empresa.setNit(sc.nextLine());
+
+            usuario = empresa;
+
+        } else if (opcion == 2) {
+            Candidato candidato = new Candidato();
+            System.out.println("Ingrese el nombre del candidato:");
+            candidato.setNombre(sc.nextLine());
+            System.out.println("Ingrese el identificador del candidato:");
+            candidato.setCedula(sc.nextLine());
+
+            usuario = candidato;
+
+        } else {
+            System.out.println("Opcion no valida.");
+            return;
+        }
+
+        usuario.nombre();
+        usuario.identificador();
+>>>>>>> Stashed changes
     }
 }
